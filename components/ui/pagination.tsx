c188b70629d3988acb7f17860e5732ecb4a6f4af
iheb-baklouti <iheb.baklouti@polytechnicien.tn>
2025-11-16 02:09:26 +0100

    fix(pagination): fix pagination

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -49,7 +49,7 @@ const PaginationLink = ({
   const Component = onClick ? 'button' : 'a';
   return (
     <Component
-      type={onClick ? ('button' as const) : undefined}
+      {...(onClick && { type: 'button' as 'button' })}
       aria-current={isActive ? 'page' : undefined}
       className={cn(
         buttonVariants({
